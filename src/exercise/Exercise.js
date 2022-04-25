d1439b2c0b6d00b5c5cbd59ce77647dcd4bf2755
DwightDubert <dwight.dubert@stu.caverna.kyschools.us>
2022-04-25 09:29:19 -0500

    react-props-exercise

---
+++ src/exercise/Exercise.js
@@ -0,0 +1,9 @@
+function Exercise({good = "Great", bad = "Not Great"}){
+    return(
+      <div>
+        <h1>{good}</h1>
+        <h1>{bad}</h1>
+      </div>
+    );
+  }
+  export default Exercise;
